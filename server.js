eb40f24d731a2d2b7474a4cb8eff3cb779288c33
nam9ming <150271811+nam9ming@users.noreply.github.com>
2025-06-01 16:38:46 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -10,6 +11,12 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  credentials: false
+}));
 
 // Use environment variables for database URL
 const dbUri = process.env.DB_URI;
